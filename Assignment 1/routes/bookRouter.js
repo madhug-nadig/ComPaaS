0df42907ad5c4b0b951aac5dc3a6459d9eb89b28
Madhu G Nadig <madhug.nadig@gmail.com>
2017-01-12 20:45:45 +0530

    Router

--- Assignment 1/routes/bookRouter.js
+++ Assignment 1/routes/bookRouter.js
@@ -7,6 +7,7 @@ var mongoose = require('mongoose');
 var books = require('../models/books');
 
 bookRouter.route('/')
+//Get all the books
 .get(function(req,res,next){
 
   books.find({}, function(err, book){
@@ -18,9 +19,10 @@ bookRouter.route('/')
 
 })
 
+//Add a new book
 .post(function (req, res, next) {
     books.create(req.body, function (err, book) {
-        if (err) throw err;
+        if (err) console.log(err);
         console.log('book created!');
         var id = book._id;
 
@@ -31,38 +33,70 @@ bookRouter.route('/')
     });
 })
 
+//Delete all with stock 0
 .delete(function (req, res, next) {
-    books.remove({}, function (err, resp) {
+    books.remove({"stock":0}, function (err, resp) {
         if (err) throw err;
         res.json(resp);
     });
 });
 
-bookRouter.route('/:bookId')
+bookRouter.route('/:bookName')
 
+//Get book by book name
 .get(function (req, res, next) {
-    books.findById(req.params.bookId, function (err, book) {
+    books.findOne({"name":req.params.bookName}, function (err, book) {
         if (err) throw err;
         res.json(book);
     });
 })
 
-.put(function (req, res, next) {
-    books.findByIdAndUpdate(req.params.bookId, {
-        $set: req.body
-    }, {
-        new: true
-    }, function (err, book) {
-        if (err) throw err;
-        res.json(book);
-    });
-})
 
+//Delete book by bookname
 .delete(function (req, res, next) {
-    books.findByIdAndRemove(req.params.bookId, function (err, resp) {
+    books.findOne({"name":req.params.bookName}, function (err, resp) {
         if (err) throw err;
         res.json(resp);
+    }).remove().exec();
+});
+
+
+bookRouter.route('/category/:categoryName')
+
+//Get all books in a category
+.get(function (req, res, next) {
+    books.find({"category":req.params.categoryName}, function (err, book) {
+        if (err) console.log(err);
+        res.json(book);
+    });
+})
+
+//Delete all books in a category
+.delete(function (req, res, next) {
+    books.findOne({"category":req.params.categoryName}, function (err, resp) {
+        if (err) throw err;
+        res.json(resp);
+    }).remove().exec();
+});
+
+bookRouter.route('/price/:bookName')
+
+//Get price of a book
+.get(function (req, res, next) {
+    books.findOne({"name":req.params.bookName}, {"price":1}, function (err, book) {
+        if (err) throw err;
+        res.json(book);
+    });
+})
+
+//Set the price of a book
+.post(function (req, res, next) {
+    console.log(req.body);
+    books.findOneAndUpdate({"name":req.params.bookName}, {"$set":{"price": req.body.price}}, function (err, resp){
+        if (err) console.log(err);
+        res.json(resp);        
     });
 });
+
 
 module.exports = bookRouter;
