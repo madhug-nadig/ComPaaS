0eec3d2f2e4542f2fb3c3971ba6d5ce318c82e6d
Nadig <madhug.nadig@gmail.com>
2017-05-11 01:17:18 +0530

    Start dynamic

--- PaaS/public/js/script.js
+++ PaaS/public/js/script.js
@@ -8,6 +8,6 @@ function createInstance(){
  postdata = {user: userid};
  url = "/create_instance";
 	$.post(url, {data : JSON.stringify(postdata)},function(data, status){
-		console.log(result);
+		console.log(data);
 	});
 }
